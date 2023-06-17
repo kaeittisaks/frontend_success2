b55bc015b00adb9c7bb92b90d71382b0582db6da
kaeittisak <jingjoe356@gmail.com>
2023-06-17 18:47:05 +0700

    first commit

---
+++ src/pages/Doctemplate.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Inject the required modules
+
+
+const GenerateContract = () => {
+ 
+
+  return (
+    <div>
+     .
+    </div>
+  );
+};
+
+export default GenerateContract;
